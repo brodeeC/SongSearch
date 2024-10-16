6e9c00af6473128fda1779fd1950db8707d3b3ed
brodeeC <brodeeclontz@gmail.com>
2024-10-16 14:15:51 -0400

    working on it

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/song")
+@app.route("/song", methods=["POST"])
 def songList():
 
     with open('top_tracks_2023.json') as file:
